dcbad6ddd1dae1b061874600e4ade8fbe68d79b3
André <vestrheim-kvist@hotmail.com>
2025-03-31 11:15:07 +0200

    array experimenting

--- index.C
+++ index.C
@@ -4,3 +4,12 @@ int main() {
     printf("Hello World!");
     return 0;
 }
+
+//Array
+#include <stdio.h>
+
+int main() {
+char myFood[][50] = {"Hamburger", "Kjøtkaker"};
+printf("%s", myFood[1]);
+return 0;
+}
